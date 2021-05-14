75107411c1b0333321ba858683e2cd8a3317de2b
Jørgen P. Tjernø <jorgenpt@gmail.com>
2021-05-14 16:04:06 -0700

    Rename binary to hermes_urls

--- Source/ThirdParty/HermesURLHandler/HermesURLHandler.Build.cs
+++ Source/ThirdParty/HermesURLHandler/HermesURLHandler.Build.cs
@@ -9,6 +9,6 @@ public class HermesURLHandler : ModuleRules
     {
         Type = ModuleType.External;
 
-		RuntimeDependencies.Add(Path.Combine("$(BinaryOutputDir)", "hermes-urls.exe"), Path.Combine(ModuleDirectory, "target", "release", "hermes-urls.exe"));
+		RuntimeDependencies.Add(Path.Combine("$(BinaryOutputDir)", "hermes_urls.exe"), Path.Combine(ModuleDirectory, "target", "release", "hermes_urls.exe"));
 	}
 }
